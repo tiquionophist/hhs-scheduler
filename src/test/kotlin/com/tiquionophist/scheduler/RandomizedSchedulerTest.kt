6ed8eaf0e6c3ad4fce67b2298fd066fb3c466120
tiquionophist <tiquionophist@gmail.com>
2021-09-23 21:08:20 -0700

    Add test for trivial schedule solution

---
+++ src/test/kotlin/com/tiquionophist/scheduler/RandomizedSchedulerTest.kt
@@ -0,0 +1,30 @@
+package com.tiquionophist.scheduler
+
+import com.tiquionophist.core.ScheduleConfiguration
+import com.tiquionophist.core.Subject
+import com.tiquionophist.core.Teacher
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+internal class RandomizedSchedulerTest {
+    @Test
+    fun testScheduleTrivial() {
+        val configuration = ScheduleConfiguration(
+            classes = 2,
+            teacherAssignments = mapOf(
+                Teacher("English", "Teacher") to setOf(Subject.ENGLISH),
+                Teacher("Math", "Teacher") to setOf(Subject.MATH),
+            ),
+            subjectFrequency = mapOf(
+                Subject.ENGLISH to 10,
+                Subject.MATH to 10,
+            )
+        )
+
+        val scheduler = RandomizedScheduler(attemptsPerRound = null, rounds = 1)
+
+        val schedule = scheduler.schedule(configuration)
+
+        assertNotNull(schedule)
+    }
+}
